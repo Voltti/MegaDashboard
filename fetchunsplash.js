d3737d7a316d1471e30605743cf30ab3d06b430c
juunis <juunisalihassan@gmail.com>
2022-08-04 16:58:58 +0300

    code to use for lambdas

--- fetchunsplash.js
+++ fetchunsplash.js
@@ -1,19 +1,20 @@
-import "dotenv/config";
 import fetch from "node-fetch";
 global.fetch = fetch;
 import { createApi } from "unsplash-js";
 
 const unsplash = createApi({
-  accessKey: dq1gfuvj9MDQcJV4WZc748Vgm - rkth8eCaba0owmzc0,
+  accessKey: "dq1gfuvj9MDQcJV4WZc748Vgm-rkth8eCaba0owmzc0",
 });
 
-unsplash.photos.getRandom({}).then((result) => {
-  if (result.errors) {
-    // handle error here
-    console.log("error occurred: ", result.errors[0]);
-  } else {
-    // handle success here
-    const photo = result.response;
-    console.log(photo);
-  }
-});
+unsplash.photos
+  .getRandom({ query: "espoo", orientation: "landscape" })
+  .then((result) => {
+    if (result.errors) {
+      // handle error here
+      console.log("error occurred: ", result.errors[0]);
+    } else {
+      // handle success here
+      const photo = result.response;
+      console.log(photo);
+    }
+  });
